b42770cbe9b80f42908761b857bbacf4783b4222
jayedHoshen <jayedkn64@gmail.com>
2024-10-31 07:38:30 +0600

    up

---
+++ Lab2/myDictionary.py
@@ -0,0 +1,9 @@
+myDictionary = {"Jayed": 76, "list": [17, 75]}
+print(myDictionary)
+
+print(myDictionary.keys())
+print(myDictionary.values())
+
+myDictionary["The Bodyguard"] = "1992"
+myDictionary["Rumours"] = "1977"
+print(myDictionary)
